b6f021f403db6c9c6a311ec9868877d25417c090
Helen <abedul@tut.by>
2019-04-19 13:49:21 +0300

    File names were corrected

--- HW_02/Elena_Terentieva/elena_terentieva_hw_02_t_01.rb
+++ HW_02/Elena_Terentieva/elena_terentieva_hw_02_t_01.rb
@@ -1,4 +1,4 @@
-# Calculating the angle betwing 
+# Calculating the angle betwing
 # the hours and the minutes hands at 3:15
 # As 3/12 = 15/60, so we need to find only
 # the angle that the hours hand moved by
@@ -9,5 +9,5 @@ minutes_hand_angle = 15.0 / 60
 angle_betwing_hands = minutes_hand_angle * hours_hand_step_per_minute
 
 puts "The angle betwing the hours and
-the minutes hands at 3:15 is 
+the minutes hands at 3:15 is
 #{(angle_betwing_hands * 360).round(2)} grad or #{(angle_betwing_hands * 2 * 3.14).round(2)} rad"
