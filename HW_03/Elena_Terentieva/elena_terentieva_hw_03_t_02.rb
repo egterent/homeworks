56a39abe5ca7f5f58b1ab95c23291588fa6f9c23
Helen <abedul@tut.by>
2019-05-09 13:42:29 +0300

    Changes in task_02

--- HW_03/Elena_Terentieva/elena_terentieva_hw_03_t_02.rb
+++ HW_03/Elena_Terentieva/elena_terentieva_hw_03_t_02.rb
@@ -16,7 +16,7 @@ def ip_match(str = '')
 end
 
 def date_match(str = '')
-  str[%r{/\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}/}]
+  str[/\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}/]
 end
 
 def address_match(str = '')
@@ -29,7 +29,7 @@ def build_string(str = '')
     date = date_match(str)
     address = address_match(str)
     if ip && date && address
-      return "#{date} FROM: #{ip} TO: #{address[%r{/\/\S+/}]}"
+      return "#{date} FROM: #{ip} TO: #{address[/\/\S+/].upcase}"
     end
   end
   ''
