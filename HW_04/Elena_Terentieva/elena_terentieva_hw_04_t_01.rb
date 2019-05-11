e7be6d204bad20169868be6033fb52909ac669ce
Helen <abedul@tut.by>
2019-05-12 01:52:28 +0300

    Changes in task 1

--- HW_04/Elena_Terentieva/elena_terentieva_hw_04_t_01.rb
+++ HW_04/Elena_Terentieva/elena_terentieva_hw_04_t_01.rb
@@ -1,9 +1,16 @@
 # returns num elements of Fibbonacci sequence
 
 def task_4_1(num = 0)
-  return [] if num < 0
+  return [] if !num.is_a?(Integer) || num < 0
   return [0] if num.zero?
 
+  fibbonacci(num)
+rescue StandardError => e
+  puts e.message
+  []
+end
+
+def fibbonacci(num)
   arr = [1]
   if num > 1
     num = 1000 if num > 1000
